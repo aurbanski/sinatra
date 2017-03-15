af93426d7232227c2e6cd10ad1c448f9d113c396
aurbanski <aurbanski27@gmail.com>
2017-03-15 11:18:38 -0600

    I finished it

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,7 +1,20 @@
 require 'sinatra'
 require 'sinatra/reloader'
+number = rand(100)
 
 get '/' do
-  number = rand(100)
-  erb :index, locals: {number: number}
+  guess = params['guess']
+  message = check_guess(guess, number)
+  erb :index, locals: {number: number, message: message}
+end
+
+def check_guess (guess, number)
+  if guess.to_i > number
+    message = "The guess was too high"
+  elsif guess.to_i < number
+    message = "The guess was too low"
+  else
+    message = "You guessed the number"
+  end
+  return message
 end
